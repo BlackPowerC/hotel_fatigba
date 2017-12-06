59b9c9e7f271ac9910658cc67c22bb1fa0a72283
fatigba <fatigba72@gmail.com>
2017-12-06 19:37:43 +0000

    ConsommationManager.java: override all abstracts methods.

--- manager/ConsommationManager.java
+++ manager/ConsommationManager.java
@@ -5,11 +5,49 @@
  */
 package manager;
 
+import bo.Consommation;
+import java.util.List;
+
 /**
  *
  * @author jordy
  */
-public class ConsommationManager
+public class ConsommationManager extends Manager<Consommation>
 {
+    @Override
+    public boolean insert(Consommation entity)
+    {
+        
+    }
+
+    @Override
+    public void delete(int id)
+    {
+        
+    }
+
+    @Override
+    public Consommation findById(int id)
+    {
+    
+    }
+
+    @Override
+    public List<Consommation> findByCriteria(String criteria, boolean strict)
+    {
+    
+    }
+
+    @Override
+    public List<Consommation> findAll()
+    {
+    
+    }
+
+    @Override
+    public int update(Consommation entity)
+    {
+    
+    }
     
 }
