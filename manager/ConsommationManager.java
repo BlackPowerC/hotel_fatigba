c288485b22b4573421b01043b0fdfacc348b80f3
fatigba <fatigba72@gmail.com>
2017-12-10 19:45:16 +0000

    ConsommationManager.java: findAll and findByCriteria methods.

--- manager/ConsommationManager.java
+++ manager/ConsommationManager.java
@@ -7,12 +7,18 @@ package manager;
 
 import bo.Client;
 import bo.Consommation;
+import bo.Nationalite;
 import bo.Service;
+import bo.Sexe;
+import bo.TypeClient;
 import com.mysql.jdbc.PreparedStatement;
 import core.Database;
 import core.Message;
 import java.io.PrintStream;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -84,20 +90,115 @@ public class ConsommationManager extends Manager<Consommation>
     @Override
     public List<Consommation> findByCriteria(String criteria, boolean strict)
     {
+        List<Consommation> list = new ArrayList<Consommation>() ;
+        Consommation co ;
+        Service se ;
+        Client n ;
+        String s = (!strict) ? "OR" : "AND";
         try
         {
-            String sql="" ;
+            String sql="SELECT c.id AS id_client, c.nom, c.prenom, c.age, c.fidele, c.etranger, c.email, c.id_nation, c.id_type, c.id_sexe, " +
+                              "co.id AS id_consommation, co.id_service, " +
+                                "se.description AS seDescription, se.prix, se.etat, " +
+                                "tc.description AS tcDescription, " +
+                                "n.nom_fr_fr, " +
+                                "S.description as sDescription"+
+                                    "FROM Client c, Consommation co, Service se, Sexe S, TypeClient tc, Nation n " +
+                                        "WHERE c.id = co.id_client AND co.id_service = se.id  " +
+                                        "AND c.id_nation = n.id " +
+                                        "AND tc.id = c.id_type " +
+                                        "AND S.id = c.id_sexe AND (c.prenom = ? "+s+" c.nom=?)" ;
+            PreparedStatement ps = (PreparedStatement) Database.getHinstance().getConnection().prepareStatement(sql) ;
+            ps.setString(1, criteria);
+            ps.setString(2, criteria);
+            ResultSet rs = ps.executeQuery() ;
+            while(rs.next())
+            {
+                n = new Client(rs.getInt("id_client"), rs.getString("nom"), rs.getString("Prenom"), rs.getString("email"), "", 
+                               new Sexe(), 
+                               new TypeClient(), 
+                               new Nationalite(), 
+                               rs.getBoolean("fidele"), 
+                               rs.getBoolean("etranger"), 
+                               new GregorianCalendar());
+                /* data bout nation */
+                n.getNation().setId(rs.getInt("id_nation"));
+                n.getNation().setDescription(rs.getString("nom_nom_fr"));
+                /* data about Sexe */
+                n.getType().setId(rs.getInt("id_type"));
+                n.getType().setDescription(rs.getString("tcDescription"));
+                /* data about Type */
+                n.getSexe().setId(rs.getInt("id_sexe"));
+                n.getSexe().setDescription(rs.getString("sDescription"));
+                list.add(
+                    new Consommation(rs, 
+                                new Service(rs.getInt("id_service"), rs.getString("seDesctiprion"), rs.getFloat("prix")),
+                                n)
+                        ) ;
+            }
+            return list ;
         }
         catch(SQLException sqlex)
         {
-            
+            //Message.error("Impossible de mettre à jour la données dont l'id vaut: "+id);
+            sqlex.printStackTrace(new PrintStream(System.err));
         }
+        return null ;
     }
 
     @Override
     public List<Consommation> findAll()
     {
-    
+        List<Consommation> list = new ArrayList<Consommation>() ;
+        Consommation co ;
+        Service se ;
+        Client n ;
+        try
+        {
+            String sql="SELECT c.id AS id_client, c.nom, c.prenom, c.age, c.fidele, c.etranger, c.email, c.id_nation, c.id_type, c.id_sexe, " +
+                              "co.id AS id_consommation, co.id_service, " +
+                                "se.description AS seDescription, se.prix, se.etat, " +
+                                "tc.description AS tcDescription, " +
+                                "n.nom_fr_fr, " +
+                                "S.description as sDescription"+
+                                    "FROM Client c, Consommation co, Service se, Sexe S, TypeClient tc, Nation n " +
+                                        "WHERE c.id = co.id_client AND co.id_service = se.id  " +
+                                        "AND c.id_nation = n.id " +
+                                        "AND tc.id = c.id_type " +
+                                        "AND S.id = c.id_sexe" ;
+            ResultSet rs = Database.getHinstance().executeQuery(sql) ;
+            while(rs.next())
+            {
+                n = new Client(rs.getInt("id_client"), rs.getString("nom"), rs.getString("Prenom"), rs.getString("email"), "", 
+                               new Sexe(), 
+                               new TypeClient(), 
+                               new Nationalite(), 
+                               rs.getBoolean("fidele"), 
+                               rs.getBoolean("etranger"),  
+                               new GregorianCalendar());
+                /* data bout nation */
+                n.getNation().setId(rs.getInt("id_nation"));
+                n.getNation().setDescription(rs.getString("nom_nom_fr"));
+                /* data about Sexe */
+                n.getType().setId(rs.getInt("id_type"));
+                n.getType().setDescription(rs.getString("tcDescription"));
+                /* data about Type */
+                n.getSexe().setId(rs.getInt("id_sexe"));
+                n.getSexe().setDescription(rs.getString("sDescription"));
+                list.add(
+                    new Consommation(rs, 
+                                new Service(rs.getInt("id_service"), rs.getString("seDesctiprion"), rs.getFloat("prix")),
+                                n)
+                        ) ;
+            }
+            return list ;
+        }
+        catch(SQLException sqlex)
+        {
+            //Message.error("Impossible de mettre à jour la données dont l'id vaut: "+id);
+            sqlex.printStackTrace(new PrintStream(System.err));
+        }
+        return null ;
     }
 
     @Override
