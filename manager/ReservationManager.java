9edd4775606700dabfd41031910f18450d866e88
jordy <jordy@jordy-HP-15-Notebook-PC>
2017-12-22 22:32:32 +0000

    ReservationManager.java: implémentation des methodes insert et delete.

--- manager/ReservationManager.java
+++ manager/ReservationManager.java
@@ -1,6 +1,12 @@
 package manager ;
 
+import bo.Chambre;
 import bo.Reservation;
+import com.mysql.jdbc.PreparedStatement;
+import core.Database;
+import core.Message;
+import java.sql.Date;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ReservationManager extends Manager<Reservation>
@@ -24,13 +30,46 @@ public class ReservationManager extends Manager<Reservation>
     @Override
     public boolean insert(Reservation entity)
     {
-        
+        if(!entity.isValid() || entity == null)
+        {
+            return false ;
+        }
+        String sql= "INSERT INTO Reservation VALUES(NULL, ?, ?, ?, ?, ?, ?)" ;
+        try
+        {
+            PreparedStatement ps = Database.getHinstance().prepare(sql);
+            ps.setInt(1, entity.getClient().getId()) ;
+            ps.setDate(3, (Date) entity.getDate().getTime());
+            ps.setDate(4, (Date) entity.getDate_arrivee().getTime());
+            ps.setDate(5, (Date) entity.getDate_fin().getTime());
+            ps.setBoolean(6, false);
+            for(Chambre ch : entity.getChambres())
+            {
+                ps.setInt(2, ch.getId());
+                ps.execute() ;
+            }
+        }
+        catch (SQLException sqlex)
+        {
+            Message.error("");
+            sqlex.printStackTrace();
+        }
+        return true;
     }
 
     @Override
     public void delete(int id)
     {
-        
+        try
+        {
+            PreparedStatement ps = Database.getHinstance().prepare("DELETE FROM Reservation WHERE id = ?") ;
+            ps.setInt(1, id) ;
+            ps.execute();
+        }catch(SQLException sqlex)
+        {
+            Message.error("");
+            sqlex.printStackTrace();
+        }
     }
 
     @Override
@@ -50,7 +89,7 @@ public class ReservationManager extends Manager<Reservation>
     {
         
     }
-
+    
     @Override
     public int update(Reservation entity)
     {
