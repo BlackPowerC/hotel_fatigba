a109493d2c40b90a96aa069367e691801e7561b7
jordy <jordy@jordy-HP-15-Notebook-PC>
2018-01-04 18:30:03 +0000

    Réimplémentation de fonction. J'ai réécris le code des fonctions findAll et findId en utilisant les manager de Service et Client, le code est devenu plus simple. La fonction findByCriteria renvoie désormais une List vide.

--- manager/ConsommationManager.java
+++ manager/ConsommationManager.java
@@ -7,10 +7,7 @@ package manager;
 
 import bo.Client;
 import bo.Consommation;
-import bo.Nationalite;
 import bo.Service;
-import bo.Sexe;
-import bo.TypeClient;
 import com.mysql.jdbc.PreparedStatement;
 import core.Database;
 import core.Message;
@@ -18,7 +15,6 @@ import java.io.PrintStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -27,6 +23,22 @@ import java.util.List;
  */
 public class ConsommationManager extends Manager<Consommation>
 {
+    private static ConsommationManager p_singleton = null ;
+    
+    private ConsommationManager()
+    {
+        
+    }
+    
+    public static ConsommationManager getInstance()
+    {
+        if(p_singleton == null)
+        {
+            p_singleton = new ConsommationManager() ;
+        }
+        return p_singleton ;
+    }
+    
     @Override
     public boolean insert(Consommation entity)
     {
@@ -57,7 +69,7 @@ public class ConsommationManager extends Manager<Consommation>
         String sql="DELETE FROM Consommation WHERE id=?";
         try
         {
-            PreparedStatement ps =  (PreparedStatement) Database.getHinstance().getConnection().prepareStatement(sql) ;
+            PreparedStatement ps = Database.getHinstance().prepare(sql) ;
             ps.setInt(1, id) ;
             ps.execute() ;
         }
@@ -74,14 +86,29 @@ public class ConsommationManager extends Manager<Consommation>
         Consommation c ;
         Service s ;
         Client cl ;
-        String sql = "";
+        String sql = "SELECT * FROM Consommation WHERE id=?" ;
         try
         {
-            PreparedStatement ps = (PreparedStatement) Database.getHinstance().getConnection().prepareStatement(sql) ;
+            PreparedStatement ps = Database.getHinstance().prepare(sql) ;
+            ps.setInt(1, id) ;
+            ResultSet rs = ps.executeQuery() ;
+            if(rs.next())
+            {
+                cl = (Client) FactoryManager.getInstance()
+                        .getManager(FactoryManager.CLIENT_MANAGER)
+                        .findById(rs.getInt("id_client")) ;
+                s = (Service) FactoryManager.getInstance()
+                        .getManager(FactoryManager.SERVICE_MANAGER)
+                        .findById(rs.getInt("id_service"));
+                c = new Consommation(id, s, cl) ;
+                return c ;
+            }
+            
+            return null ;
         }
         catch(SQLException sqlex)
         {
-            Message.error("Impossible de supprimer la données dont l'id vaut: "+id);
+            Message.error(sqlex.getMessage()+ " !");
             sqlex.printStackTrace(new PrintStream(System.err));
         }
         return null;
@@ -90,115 +117,35 @@ public class ConsommationManager extends Manager<Consommation>
     @Override
     public List<Consommation> findByCriteria(String criteria, boolean strict)
     {
-        List<Consommation> list = new ArrayList<Consommation>() ;
-        Consommation co ;
-        Service se ;
-        Client n ;
-        String s = (!strict) ? "OR" : "AND";
-        try
-        {
-            String sql="SELECT c.id AS id_client, c.nom, c.prenom, c.age, c.fidele, c.etranger, c.email, c.id_nation, c.id_type, c.id_sexe, " +
-                              "co.id AS id_consommation, co.id_service, " +
-                                "se.description AS seDescription, se.prix, se.etat, " +
-                                "tc.description AS tcDescription, " +
-                                "n.nom_fr_fr, " +
-                                "S.description as sDescription"+
-                                    "FROM Client c, Consommation co, Service se, Sexe S, TypeClient tc, Nation n " +
-                                        "WHERE c.id = co.id_client AND co.id_service = se.id  " +
-                                        "AND c.id_nation = n.id " +
-                                        "AND tc.id = c.id_type " +
-                                        "AND S.id = c.id_sexe AND (c.prenom = ? "+s+" c.nom=?)" ;
-            PreparedStatement ps = (PreparedStatement) Database.getHinstance().getConnection().prepareStatement(sql) ;
-            ps.setString(1, criteria);
-            ps.setString(2, criteria);
-            ResultSet rs = ps.executeQuery() ;
-            while(rs.next())
-            {
-                n = new Client(rs.getInt("id_client"), rs.getString("nom"), rs.getString("Prenom"), rs.getString("email"), "", 
-                               new Sexe(), 
-                               new TypeClient(), 
-                               new Nationalite(), 
-                               rs.getBoolean("fidele"), 
-                               rs.getBoolean("etranger"), 
-                               new GregorianCalendar());
-                /* data bout nation */
-                n.getNation().setId(rs.getInt("id_nation"));
-                n.getNation().setDescription(rs.getString("nom_nom_fr"));
-                /* data about Sexe */
-                n.getType().setId(rs.getInt("id_type"));
-                n.getType().setDescription(rs.getString("tcDescription"));
-                /* data about Type */
-                n.getSexe().setId(rs.getInt("id_sexe"));
-                n.getSexe().setDescription(rs.getString("sDescription"));
-                list.add(
-                    new Consommation(rs, 
-                                new Service(rs.getInt("id_service"), rs.getString("seDesctiprion"), rs.getFloat("prix")),
-                                n)
-                        ) ;
-            }
-            return list ;
-        }
-        catch(SQLException sqlex)
-        {
-            //Message.error("Impossible de mettre à jour la données dont l'id vaut: "+id);
-            sqlex.printStackTrace(new PrintStream(System.err));
-        }
-        return null ;
+        return new ArrayList<Consommation>();
     }
 
     @Override
     public List<Consommation> findAll()
     {
-        List<Consommation> list = new ArrayList<Consommation>() ;
-        Consommation co ;
-        Service se ;
-        Client n ;
+        List<Consommation> list = new ArrayList<>() ;
+        Consommation c ;
+        Service s ;
+        Client cl ;
+        String sql = "SELECT * FROM Consommation" ;
         try
         {
-            String sql="SELECT c.id AS id_client, c.nom, c.prenom, c.age, c.fidele, c.etranger, c.email, c.id_nation, c.id_type, c.id_sexe, " +
-                              "co.id AS id_consommation, co.id_service, " +
-                                "se.description AS seDescription, se.prix, se.etat, " +
-                                "tc.description AS tcDescription, " +
-                                "n.nom_fr_fr, " +
-                                "S.description as sDescription"+
-                                    "FROM Client c, Consommation co, Service se, Sexe S, TypeClient tc, Nation n " +
-                                        "WHERE c.id = co.id_client AND co.id_service = se.id  " +
-                                        "AND c.id_nation = n.id " +
-                                        "AND tc.id = c.id_type " +
-                                        "AND S.id = c.id_sexe" ;
             ResultSet rs = Database.getHinstance().executeQuery(sql) ;
             while(rs.next())
             {
-                n = new Client(rs.getInt("id_client"), rs.getString("nom"), rs.getString("Prenom"), rs.getString("email"), "", 
-                               new Sexe(), 
-                               new TypeClient(), 
-                               new Nationalite(), 
-                               rs.getBoolean("fidele"), 
-                               rs.getBoolean("etranger"),  
-                               new GregorianCalendar());
-                /* data bout nation */
-                n.getNation().setId(rs.getInt("id_nation"));
-                n.getNation().setDescription(rs.getString("nom_nom_fr"));
-                /* data about Sexe */
-                n.getType().setId(rs.getInt("id_type"));
-                n.getType().setDescription(rs.getString("tcDescription"));
-                /* data about Type */
-                n.getSexe().setId(rs.getInt("id_sexe"));
-                n.getSexe().setDescription(rs.getString("sDescription"));
-                list.add(
-                    new Consommation(rs, 
-                                new Service(rs.getInt("id_service"), rs.getString("seDesctiprion"), rs.getFloat("prix")),
-                                n)
-                        ) ;
+                list.add(new Consommation(rs ,
+                    (Service) FactoryManager.getInstance().getManager(FactoryManager.SERVICE_MANAGER).findById(rs.getInt("id_service")),
+                    (Client) FactoryManager.getInstance().getManager(FactoryManager.CLIENT_MANAGER).findById(rs.getInt("id_client"))
+                ));
             }
             return list ;
         }
         catch(SQLException sqlex)
         {
-            //Message.error("Impossible de mettre à jour la données dont l'id vaut: "+id);
+            Message.error(sqlex.getMessage()+ " !");
             sqlex.printStackTrace(new PrintStream(System.err));
         }
-        return null ;
+        return null;
     }
 
     @Override
@@ -211,7 +158,7 @@ public class ConsommationManager extends Manager<Consommation>
         String sql="UPDATE Consommation SET id_client=?, id_service=? WHERE id=?";
         try
         {
-            PreparedStatement ps =  (PreparedStatement) Database.getHinstance().getConnection().prepareStatement(sql) ;
+            PreparedStatement ps = Database.getHinstance().prepare(sql) ;
             ps.setInt(1, entity.getClient().getId()) ;
             ps.setInt(1, entity.getService().getId()) ;
             ps.setInt(1, entity.getId()) ;
