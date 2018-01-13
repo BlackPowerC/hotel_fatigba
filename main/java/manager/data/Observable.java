903874972b8d999b6b8b766d52f5350a123c86f9
jordy <jordy@jordy-HP-15-Notebook-PC>
2018-01-13 12:13:20 +0000

    Passage vers une structure maven

---
+++ main/java/manager/data/Observable.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manager.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import userInterface.Observateur;
+
+/**
+ *
+ * @author jordy
+ */
+public abstract class Observable
+{
+    protected List<Observateur> lo ;
+    
+    public Observable()
+    {
+        lo = new ArrayList<>();
+    }
+    
+    public void addObservateur(Observateur o)
+    {
+        lo.add(o) ;
+    }
+    
+    public void removeObservateur(Observateur o)
+    {
+        lo.remove(o) ;
+    }
+       
+    public void notifier()
+    {
+        lo.forEach((o) ->
+        {
+            o.update();
+        });
+    }
+}
