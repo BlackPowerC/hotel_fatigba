5edb5a5f83d68509163768ad55b7cc76ee40baa3
jordy <jordy@jordy-HP-15-Notebook-PC>
2018-01-24 20:36:08 +0000

    PanelAdmin Dans la classe UserPanel, j'ai ajouter des champs. Les bouttons de controles et leur box verticales. J'ai ajouter un constructeur public. J'ai aussi rajouter la méthode addContent qui ajoute tout les widgets au panel. J'ai fini le code de la methode buildField.

--- main/java/gui/admin/panel/UserPanel.java
+++ main/java/gui/admin/panel/UserPanel.java
@@ -5,10 +5,16 @@
  */
 package main.java.gui.admin.panel;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import main.java.app.Buttons;
 
 /**
  *
@@ -17,61 +23,121 @@ import javax.swing.JTextField;
  */
 public class UserPanel
 {
+
     /**
-     * Cette classe représente un formulaire permettant de manipuler les utilisateurs.
+     * Cette classe représente un formulaire permettant de manipuler les
+     * utilisateurs.
      */
-    private class Form
+    public class Form
     {
+
         /* Les champs du formulaire */
-        private JTextField txtNom; 
-        private JLabel labelNom; 
-        
-        private JTextField txtPrenom; 
-        private JLabel labelPrenom; 
-        
-        private JTextField txtEmail; 
+        private JTextField txtNom;
+        private JLabel labelNom;
+
+        private JTextField txtPrenom;
+        private JLabel labelPrenom;
+
+        private JTextField txtEmail;
         private JLabel labelEmail;
-        
-        private JComboBox<String> comboSexe; 
+
+        private JComboBox<String> comboSexe;
         private JLabel labelSexe;
-        
+
         private JComboBox<String> comboType;
-        private JLabel labelType; 
-        
+        private JLabel labelType;
+
         private JPasswordField txtPasswd;
         private JLabel labelPasswd;
+
+        /* Les contrôls */
+        private Buttons buttons;
+        private Box hBox1;
+        private Box hBox2;
+
+        /* Les Layout */
+        private GridLayout gridLayout;
+        private JPanel panel;
+
+        public Form()
+        {
+            this.buttons = new Buttons();
+            this.hBox1 = Box.createHorizontalBox();
+            this.hBox2 = Box.createHorizontalBox();
+            this.gridLayout = new GridLayout(7, 2, 5, 8);
+            this.panel.setLayout(this.gridLayout);
+        }
         
+        /**
+         * Cette fonction ajoute des composants SWING dans le panel.
+         */
+        public void addContent()
+        {
+            /* Ajout des contrôls */
+            for (int i = 0; i <= 1; i++)
+            {
+                this.hBox1.add(buttons.getButtons(i));
+                this.hBox2.add(buttons.getButtons(i + 2));
+            }
+            this.panel.add(txtNom);
+            this.panel.add(labelNom);
+
+            this.panel.add(txtPrenom);
+            this.panel.add(labelPrenom);
+
+            this.panel.add(txtEmail);
+            this.panel.add(labelEmail);
+
+            this.panel.add(comboSexe);
+            this.panel.add(labelSexe);
+
+            this.panel.add(comboType);
+            this.panel.add(labelType);
+
+            this.panel.add(txtPasswd);
+            this.panel.add(labelPasswd);
+            
+            this.panel.add(hBox1) ;
+            this.panel.add(hBox2) ;
+        }
+
         /**
          * Cette fonction instancie les objets JLabel de la classe.
          */
         private void buildLabel()
         {
-            labelNom = new JLabel("Nom") ;
-            labelPrenom = new JLabel("Prénom") ;
-            labelEmail = new JLabel("E-mail") ;
-            labelType = new JLabel("Type d'utilisateur") ;
-            labelPasswd = new JLabel("Mot de passe") ;
-            labelSexe = new JLabel("Sexe") ;
+            labelNom = new JLabel("Nom");
+            labelPrenom = new JLabel("Prénom");
+            labelEmail = new JLabel("E-mail");
+            labelType = new JLabel("Type d'utilisateur");
+            labelPasswd = new JLabel("Mot de passe");
+            labelSexe = new JLabel("Sexe");
         }
-        
+
         /**
-         * Cette fonction instancie les objets du formulaire oû l'on entre des
+         * Cette fonction instancie les champs de texte du formulaire oû l'on entre des
          * données.
          */
         private void buildField()
         {
-            txtNom = new JTextField() ;
+            txtNom = new JTextField();
             txtNom.setToolTipText("Nom de l'utilisateur");
-            
+
             txtPrenom = new JTextField();
             txtPrenom.setToolTipText("Prénom de l'utilisateur");
-                    
-            txtEmail = new JTextField() ;
-            txtEmail.setToolTipText("E-mail de l'utilisateur") ;
-                    
-            txtPasswd = new JPasswordField() ;
-            comboSexe = new JComboBox<>(new String[]{"", "homme", "femme"}) ;
-            comboType = new JComboBox<>(new String[]{"", "clientAdmin", "reservationAdmin", "admin"}) ;
+
+            txtEmail = new JTextField();
+            txtEmail.setToolTipText("E-mail de l'utilisateur");
+
+            txtPasswd = new JPasswordField();
+            comboSexe = new JComboBox<>(new String[]
+            {
+                "", "homme", "femme"
+            });
+            comboType = new JComboBox<>(new String[]
+            {
+                "", "clientAdmin", "reservationAdmin", "admin"
+            });
         }
     }
 }
